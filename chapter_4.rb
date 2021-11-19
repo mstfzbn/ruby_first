4353004ee3b76280a6ab4b06df360aa44361cc3e
Mustafa Zaban <8656319+mstfzbn@users.noreply.github.com>
2021-11-19 10:28:25 +0200

    update befor first test

--- chapter_4.rb
+++ chapter_4.rb
@@ -56,6 +56,11 @@ h['cow']  = 'bovine'
 #perhaps deserible format is
 #h = { dog: 'canine', cat: 'feline', donkey: 'asinine' } | literal, writed by colons at the and of key symbol and dropped => 
 
+#for cycle example
+for i in 0...8
+    #something
+end
+
 
 #chek again page 49
 #check this string.downcase.scan( for_something ) two functions together
@@ -63,8 +68,28 @@ def words_from_string(string)
     string.downcase.scan(/[\w']+/)
 end
 
-#for cycle example
+def count_frequency(word_list)
+    counts = Hash.new(0)
 
-for i in 0...8
-    p i
+    for word in word_list
+      counts[word] += 1
+    end
+
+    counts
+end
+
+raw_text = %{The problem breaks down into two parts. First, given some text
+    as a string, return a list of words. That sounds like an array. Then, build
+    a count for each distinct word. That sounds like a use for a hash---we can
+    index it with the word and use the corresponding entry to keep a count.}
+   
+word_list = words_from_string(raw_text)
+counts = count_frequency(word_list)
+sorted = counts.sort_by {|word, count| count}
+top_five = sorted.last(5)
+
+for i in 0...5              # (this is ugly code--read on
+   word = top_five[i][0]    # for a better version)
+   count = top_five[i][1]
+   puts "#{word}: #{count}"
 end
