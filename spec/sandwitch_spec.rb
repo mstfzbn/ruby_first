4f73418233a216880f0c9553d946b81a70f867b0
Mustafa Zaban <8656319+mstfzbn@users.noreply.github.com>
2021-11-29 15:22:42 +0200

    Add files via upload

--- spec/sandwitch_spec.rb
+++ spec/sandwitch_spec.rb
@@ -1,15 +1,31 @@
-Sandwich = Struct.new(:taste, :toppings)
+require '../sandwitch_primitive.rb'
 
-#describe, it, expect are part of rspec sore api
+#describe, it, expect are part of rspec core api
 #rspec command in terminal, started from projects paret dir
 #will search and run every *_spec.rb file in spec folder
 
+#RSpec hooks
+#Helper methods
+#RSpec's let
+
 RSpec.describe 'An ideal sandwich' do #example group 
+
+    before { @sandwich = Sandwich.new('delicious', []) } #it's a hook
+   
     it 'is delicious' do  #an example 
-     sandwich = Sandwich.new('delicious', [])
     
-     taste = sandwich.taste
+        taste = @sandwich.taste
     
-     expect(taste).to eq('delicious') #an assertion
+        expect(taste).to eq('delicious') #an assertion
     end
+
+    it 'sandwitch with toppings' do
+
+        @sandwich.toppings << 'cheese'
+
+        toppings = @sandwich.toppings
+
+        expect(toppings).not_to be_empty #check
+    end
+
 end
